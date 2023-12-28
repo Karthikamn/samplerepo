bbab20faf087d6b69377e6556e3174841a9c8668
Karthika <skarthika@LP-5CD2081L0B.HCLT.CORP.HCL.IN>
2023-12-28 16:24:22 +0530

    first commit

---
+++ SelFramework/src/test/java/com/selenium/testCases/First.java
@@ -0,0 +1,14 @@
+package com.selenium.testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+	
+	
+	public void test() {
+		WebDriver driver=new ChromeDriver();
+		
+	}
+
+}
